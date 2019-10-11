665a6cc497c1fddc2db8471366057d4b4c3ca8ab
Chimpook Virtuch <chimpook@gmail.com>
2019-10-11 18:02:38 +0300

    Init commit. Copy framework files.

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+// DB parameters
+define('DB_HOST', 'localhost');
+define('DB_USER', 'YOUR_USERNAME');
+define('DB_PASS', 'YOUR_PASSWORD');
+define('DB_NAME', 'YOUR_DBNAME');
+
+// App root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL root
+define('URLROOT', 'YOUR_URL');
+
+// Site name
+define('SITENAME', 'YOUR_SITENAME');
---
+++ app/libraries/Core.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * App Core Class
+ * Creates URL & loads core controller
+ * URL Format: /controller/method/params
+ */
+class Core 
+{
+    protected $currentControllerName = 'Pages';
+    protected $currentMethodName = 'index';
+    protected $params = [];
+    protected $currentController;
+
+    public function __construct() 
+    {
+        // print_r($this->getUrl());
+
+        $url = $this->getUrl();
+
+        // Look in controllers for first value
+        if (file_exists('../app/controllers/' 
+            . ucwords($url[0]) . '.php')) {
+            // If exists, set as controller
+            $this->currentControllerName = ucwords($url[0]);
+            
+            // Unset 0 Index
+            unset($url[0]);
+        }
+
+        // Require the controller
+        require_once '../app/controllers/' 
+            . $this->currentControllerName . '.php';
+
+        // Instantiate controller class
+        $this->currentController = new $this->currentControllerName;
+
+        // Check for second part of the URL
+        if (isset($url[1])) {
+            // Check to see if method exists in controller
+            if (method_exists($this->currentController, $url[1])) {
+                $this->currentMethodName = $url[1];
+            }
+            // Unset 1 Index
+            unset($url[1]);
+        }
+        
+        // Get params
+        $this->params = $url  ? array_values($url) : [];
+
+        // Call a callback with array of params
+        call_user_func_array([
+            $this->currentController,
+            $this->currentMethodName
+        ], $this->params);
+    }
+
+    public function getUrl() 
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+
+            return $url;
+        }
+    }
+}
