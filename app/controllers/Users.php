395c083768d187b24b2e7489f7dddc3364ce09bd
Сергей Веселовский <s.veselovskiy@estp.ru>
2019-10-18 18:15:06 +0300

    34. User login

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -121,10 +121,26 @@ class Users extends Controller
                 $data['password_err'] = 'Please enter password';
             }
 
+            // Check for user/email
+            if (!$this->userModel->findUserByEmail($data['email'])) {
+                $data['email_err'] = 'No user found';
+            }
+
             // Make shure errors are empty
             if (empty($data['email_err']) && empty($data['password_err'])) {
-                // validated
-                die('SUCCESS');
+                // Validated
+                // Check and set logged in user
+                $loggedInUser = $this->userModel->login($data['email'], $data['password']);
+
+                if ($loggedInUser) {
+                    // Create Session
+                    die('SUCCESS');
+                } else {
+                    $data['password_err'] = 'Password incorrect';
+
+                    $this->view('users/login', $data);
+                }
+
             } else {
                 // Load view with errors
                 $this->view('users/login', $data);
