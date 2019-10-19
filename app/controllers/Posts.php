eba6fddceecefb97059de127a321ef316f5971c7
Sergey Veselovskiy <chimpook@gmail.com>
2019-10-19 19:32:47 +0300

    41. Post show details page

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -8,6 +8,7 @@ class Posts extends Controller
         }
 
         $this->postModel = $this->model('Post');
+        $this->userModel = $this->model('User');
     }
 
     public function index()
@@ -64,5 +65,16 @@ class Posts extends Controller
             ];
             $this->view('posts/add', $data);
         }
+    }
+
+    public function show($id)
+    {
+        $post = $this->postModel->getPostById($id);
+        $user = $this->userModel->getUserById($post->user_id);
+        $data = [
+            'post' => $post,
+            'user' => $user
+        ];
+        $this->view('posts/show', $data);
     }
 }
